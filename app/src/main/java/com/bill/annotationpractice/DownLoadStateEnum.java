2c4009196e947f9eec6da5a1a6b7660fb0dece64
袁卫彪 <ybilland@163.com>
2018-07-29 14:46:58 +0800

    枚举注解

---
+++ app/src/main/java/com/bill/annotationpractice/DownLoadStateEnum.java
@@ -0,0 +1,17 @@
+package com.bill.annotationpractice;
+
+/**
+ * Created by Bill on 2018/7/29.
+ * <p>
+ * 枚举
+ */
+
+public enum DownLoadStateEnum {
+
+    NONE,
+    WAIT,
+    DOWNLOADING,
+    COMPLETE,
+    ERROR
+
+}
---
+++ app/src/main/java/com/bill/annotationpractice/DownloadStatusConst.java
@@ -0,0 +1,15 @@
+package com.bill.annotationpractice;
+
+/**
+ * Created by Bill on 2018/7/29.
+ * <p>
+ * 常量
+ */
+
+public class DownloadStatusConst {
+    public static final int NONE = 0;
+    public static final int WAIT = 1;
+    public static final int DOWNLOADING = 2;
+    public static final int COMPLETE = 3;
+    public static final int ERROR = 4;
+}
